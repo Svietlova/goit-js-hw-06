7841b916ee1fd421c191dcce18595fbd0e632424
Svietlova <irasvet1997@gmail.com>
2024-01-16 20:53:03 +0200

    FS_group_100-JS-homework

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const line = document.querySelector('#font-size-control')
-const text = document.querySelector('#text')
-const numberLine = line.value
-text.style.fontSize = `${numberLine}px`
-line.addEventListener('input', () => {
-    const number = line.value
-    text.style.fontSize = `${number}px`
-})
